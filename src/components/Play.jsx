ddd7a7291d52f8ac2a8147eb785c2d8e4ae86753
Tish Chungoora <tishal82@gmail.com>
2020-01-11 12:54:42 +0000

    Included script tag for ResponsiveVoice API in index.html

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -4,7 +4,7 @@ export default class Play extends Component {
   render() {
     return (
       <div className="Play row justify-content-center">
-        <button className="btn btn-warning">
+        <button className="btn btn-warning" onClick={() => window.responsiveVoice.speak('Hello World')}>
             <span className="oi oi-media-play"></span>
           </button>
       </div>
