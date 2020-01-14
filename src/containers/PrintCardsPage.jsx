c142a31ca91b937d55e6ceacb0e09862f15fe3a1
Tish Chungoora <tishal82@gmail.com>
2020-01-14 15:58:29 +0000

    Placeholder for print cards page modified

--- src/containers/PrintCardsPage.jsx
+++ src/containers/PrintCardsPage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PecsContainer from "./PecsContainer";
 import PecsBoard from "./PecsBoard";
-import Player from "../components/Player";
+// import Player from "../components/Player";
 import API from "../adapters/API";
 
 export default class PecsBoardPage extends Component {
@@ -11,9 +11,10 @@ export default class PecsBoardPage extends Component {
     categories: [],
     subcategories: [],
     searchTerm: "",
+    noSearchResults: false,
     methodSwitch: false,
-    cardsOnBoard: [],
-    played: false
+    cardsOnBoard: []
+    // played: false
   };
 
   setCategories = () => {
@@ -34,6 +35,7 @@ export default class PecsBoardPage extends Component {
   };
 
   componentDidMount() {
+    window.scrollTo(0, 0);
     this.setCategories();
     this.setCards();
   }
@@ -71,19 +73,29 @@ export default class PecsBoardPage extends Component {
       searchTerm: event.target.value
     });
 
+  searchForPotentialMatching = () => {
+    const newCards = this.state.cards.filter(
+      c =>
+        c.title.toLowerCase().includes(this.state.searchTerm) ||
+        c.category.name.toLowerCase().includes(this.state.searchTerm)
+    );
+    if (newCards.length < 1) {
+      this.setState({ noSearchResults: true });
+    } else {
+      this.setState({
+        displayedCards: newCards,
+        noSearchResults: false
+      });
+    }
+  };
+
   handleSearchSubmit = event => {
     event.preventDefault();
 
     if (this.state.searchTerm === "") {
       this.setState({ displayedCards: this.state.cards });
     } else {
-      this.setState({
-        displayedCards: this.state.cards.filter(
-          c =>
-            c.title.toLowerCase().includes(this.state.searchTerm) ||
-            c.category.name.toLowerCase().includes(this.state.searchTerm)
-        )
-      });
+      this.searchForPotentialMatching();
     }
   };
 
@@ -91,28 +103,32 @@ export default class PecsBoardPage extends Component {
     event.preventDefault();
     this.setState({
       searchTerm: "",
-      displayedCards: this.state.cards
+      displayedCards: this.state.cards,
+      noSearchResults: false
     });
   };
 
   methodChange = () => {
-    this.setState({ methodSwitch: !this.state.methodSwitch });
+    this.setState({
+      methodSwitch: !this.state.methodSwitch,
+      noSearchResults: false
+    });
   };
 
   selectCard = card => {
-    if (this.state.cardsOnBoard.includes(card)) {
-      alert(
-        "You can only add a card once to a PECS board. Please add a different card."
-      );
-    } else if (this.state.cardsOnBoard.length < 6) {
-      this.setState({
-        cardsOnBoard: [...this.state.cardsOnBoard, card]
-      });
-    } else {
-      alert(
-        "A PECS board can hold a maximum of 6 cards at a time. This is so that information can be exchanged as efficiently as possible, especially to non-verbal individuals with autism."
-      );
-    }
+    // if (this.state.cardsOnBoard.includes(card)) {
+    //   alert(
+    //     "You can only add a card once to a PECS board. Please add a different card."
+    //   );
+    // } else if (this.state.cardsOnBoard.length < 6) {
+    this.setState({
+      cardsOnBoard: [...this.state.cardsOnBoard, card]
+    });
+    // } else {
+    //   alert(
+    //     "A PECS board can hold a maximum of 6 cards at a time. This is so that information can be exchanged as efficiently as possible, especially to non-verbal individuals with autism."
+    //   );
+    // }
   };
 
   removeCard = card => {
@@ -124,24 +140,24 @@ export default class PecsBoardPage extends Component {
     this.setState({ cardsOnBoard: [] });
   };
 
-  playVoice = () => {
-    let wordSet = this.state.cardsOnBoard.map(card => card.title).join(", ");
-    window.responsiveVoice.enableEstimationTimeout = false;
-    window.responsiveVoice.speak(wordSet, "UK English Female", {
-      rate: 0.75,
-      onend: this.toggleToPlay
-    });
-    this.setState({ played: true });
-  };
+  // playVoice = () => {
+  //   let wordSet = this.state.cardsOnBoard.map(card => card.title).join(", ");
+  //   window.responsiveVoice.enableEstimationTimeout = false;
+  //   window.responsiveVoice.speak(wordSet, "UK English Female", {
+  //     rate: 0.75,
+  //     onend: this.toggleToPlay
+  //   });
+  //   this.setState({ played: true });
+  // };
 
-  stopVoice = () => {
-    window.responsiveVoice.cancel();
-    this.setState({ played: false });
-  };
+  // stopVoice = () => {
+  //   window.responsiveVoice.cancel();
+  //   this.setState({ played: false });
+  // };
 
-  toggleToPlay = () => {
-    this.setState({ played: false });
-  };
+  // toggleToPlay = () => {
+  //   this.setState({ played: false });
+  // };
 
   render() {
     const {
@@ -149,9 +165,10 @@ export default class PecsBoardPage extends Component {
       subcategories,
       displayedCards,
       searchTerm,
+      noSearchResults,
       methodSwitch,
-      cardsOnBoard,
-      played
+      cardsOnBoard
+      // played
     } = this.state;
     const {
       handleCategoryChange,
@@ -161,14 +178,14 @@ export default class PecsBoardPage extends Component {
       handleSearchClear,
       methodChange,
       selectCard,
-      playVoice,
-      stopVoice,
+      // playVoice,
+      // stopVoice,
       removeCard,
       resetBoard
     } = this;
 
     return (
-      <div className="PecsBoardPage container-fluid">
+      <div className="PrintCardsPage container-fluid">
         <div className="PecsBoardArea row justify-content-center pt-3 mb-3">
           <PecsBoard
             cardsOnBoard={cardsOnBoard}
@@ -176,9 +193,9 @@ export default class PecsBoardPage extends Component {
             resetBoard={resetBoard}
           />
         </div>
-        <div>
+        {/* <div>
           <Player playVoice={playVoice} stopVoice={stopVoice} played={played} />
-        </div>
+        </div> */}
         <hr className="mb-5" />
         <div>
           <PecsContainer
@@ -186,6 +203,7 @@ export default class PecsBoardPage extends Component {
             handleSearchSubmit={handleSearchSubmit}
             handleSearchClear={handleSearchClear}
             searchTerm={searchTerm}
+            noSearchResults={noSearchResults}
             methodChange={methodChange}
             methodSwitch={methodSwitch}
             categories={categories}
