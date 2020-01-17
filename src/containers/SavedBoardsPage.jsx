1e4130070341c93bbeee8ffdf069895e7df3b33c
Tish Chungoora <tishal82@gmail.com>
2020-01-17 21:08:23 +0000

    Implemented get request to boards url to save as state in SavedBoardsPage

--- src/containers/SavedBoardsPage.jsx
+++ src/containers/SavedBoardsPage.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
+import API from "../adapters/API";
 
 export default class SavedBoardsPage extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Saved boards go here!</h1>
-            </div>
-        )
-    }
+  state = {
+    boards: []
+  };
+
+  setBoards = () => {
+    API.fetchBoards().then(data =>
+      this.setState({
+        boards: data.sort((a, b) =>
+          a.title.localeCompare(b.title))
+      })
+    );
+  };
+
+  componentDidMount() {
+      window.scrollTo(0, 0)
+      this.setBoards()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Saved boards go here!</h1>
+      </div>
+    );
+  }
 }
