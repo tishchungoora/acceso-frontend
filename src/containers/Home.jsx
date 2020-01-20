9daa85be984c6a43ad94543e06b51145b7043f45
Tish Chungoora <tishal82@gmail.com>
2020-01-20 17:04:53 +0000

    Styling improvements on homepage to make key messages stand out

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -6,28 +6,62 @@ export default class Home extends Component {
     return (
       <div className="Home container-fluid">
         <div id="banner" className="row justify-content-center mb-4">
-          <img className="container-fluid" src={require('../assets/banner.jpeg')} alt="Acceso banner" />
+          <img
+            className="container-fluid"
+            src={require("../assets/banner.jpeg")}
+            alt="Acceso banner"
+          />
           <p>Source: https://pecsusa.com/</p>
         </div>
-        <div className="row justify-content-center">
-          <div className="col-sm-10 mb-2">
-            <h2>The web app for communicating intention and emotion with cards</h2>
-            <p>
-              <b>acceso</b> is an easy-to-use web application aimed primarily at parents
-              and carers needing to communicate intention and emotion with
-              non-verbal autistic individuals.
-            </p>
-            <p>
-              <b>acceso</b> can also be used as an educational and communication
-              tool by young children who are yet to fully develop speech but
-              capable of communicating through visual methods.
-            </p>
-            <p>
-              <b>acceso</b> provides a set of Picture Exchange Communication
-              System (PECS) cards. The cards can be laid out on a board, in
-              small batches at a time, and the app is then able to play back the
-              sequence of the PECS cards in audio form.
-            </p>
+        <div className="row justify-content-center m-2">
+          <div className="row text-center">
+            <h2>
+              The no. 1 web app for communicating intention and emotion with
+              cards
+            </h2>
+          </div>
+          <div className="row m-2 mt-4">
+            <div className="col-sm-4">
+              <p>
+                <b>acceso</b> is an easy-to-use web application aimed primarily
+                at parents and carers needing to communicate intention and
+                emotion with non-verbal autistic individuals.
+              </p>
+            </div>
+            <div className="col-sm-4">
+              <div className="row">
+                <div className="col-sm-1">
+                  <h1 className="text-success">&#10122;</h1>
+                </div>
+                <div className="col-sm-10 ml-1 mt-2">
+                  <p>Select PECS cards to place on a board</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-1">
+                  <h1 className="text-success">&#10123;</h1>
+                </div>
+                <div className="col-sm-10 ml-1 mt-2">
+                  <p>Show the board or play the audio to communicate</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-sm-1">
+                  <h1 className="text-success">&#10124;</h1>
+                </div>
+                <div className="col-sm-10 ml-1 mt-2">
+                  <p>Save the board for quick access in future</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <p>
+                <b>acceso</b> can also be used as an educational and
+                communication tool by young children who are yet to fully
+                develop speech but capable of communicating through visual
+                methods.
+              </p>
+            </div>
           </div>
         </div>
         <hr />
@@ -45,7 +79,7 @@ export default class Home extends Component {
         <hr />
         <div className="container-fluid mt-5 mb-5">
           <div className="row justify-content-center">
-            <h1 className="display-5 text-info mb-4">
+            <h1 className="display-5 text-info mb-4 text-center">
               Ready to build your first PECS board?
             </h1>
           </div>
--- src/containers/PecsBoard.jsx
+++ src/containers/PecsBoard.jsx
@@ -14,12 +14,12 @@ export default class PecsBoard extends Component {
         <div className="row justify-content-center m-2">
           {cardsOnBoard.length === 0 ? (
             <p className="lead text-center">
-              &#9312; Select up to 6 PECS cards by searching or filtering by
+              <i className="fas fa-arrow-circle-right"></i> Select up to 6 PECS cards by searching or filtering by
               category
             </p>
           ) : (
             <p className="lead text-center">
-              &#9313; Play the sequence, reset or save your board
+              <i className="fas fa-arrow-circle-right"></i> Show the board, play the sequence, reset or save
             </p>
           )}
           
