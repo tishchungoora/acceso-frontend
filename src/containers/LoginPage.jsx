47712fec1e26891afd2e4048e38b297ea7cc2cc8
Tish Chungoora <tishal82@gmail.com>
2020-01-22 17:24:04 +0000

    Made provision for guests to use a test user account to log in and explore the app

--- src/containers/LoginPage.jsx
+++ src/containers/LoginPage.jsx
@@ -20,12 +20,31 @@ export default class LoginPage extends Component {
     this.props.history.push("/");
   };
 
+  handleTestAccountDetail = () => {
+    let email = document.getElementById("email");
+    email.value = "j.doe@gmail.com";
+    let password = document.getElementById("password");
+    password.value = "833Fw.U7*KsM";
+    this.setState({
+      loginData: { email: email.value, password: password.value }
+    });
+  };
+
   render() {
     return (
       <div className="LoginPage m-5">
         <div className="row justify-content-center">
           <div className="login-container mt-5 col-sm-4">
-            <h2 className="text-center mb-5">Log into your account</h2>
+            <h2 className="text-center">Log into your account</h2>
+            <div className="row justify-content-center mb-5">
+              <button
+                type="submit"
+                className="btn btn-link"
+                onClick={() => this.handleTestAccountDetail()}
+              >
+                Use test account
+              </button>
+            </div>
             <form className="login-form" onSubmit={this.handleLoginSubmit}>
               <div className="form-group">
                 <label htmlFor="email">Email address</label>
